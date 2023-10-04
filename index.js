78ce78f1e0d90249cf75b6b31e374572243a03b0
George vila <gvila92@gmail.com>
2023-10-04 09:54:51 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(a, b) {
+    return a/b;
+}
+
+const result1 = divide(2000, 100);
+console.log(result1);
+
+const square = (x) => {
+    return x * x;
+}
+
+const add = (a, b) => {
+    return a + b;
+}
